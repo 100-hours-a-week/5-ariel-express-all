acd566e5dbf64d64dca43f5adb32a2f7db611a14
yeji0214 <1225yj50@naver.com>
2024-06-09 22:17:55 +0900

    [Change CSS] sign-in page

--- backend/model/db.js
+++ backend/model/db.js
@@ -3,9 +3,9 @@ import mysql from 'mysql2/promise';
 // MySQL 데이터베이스 연결 설정
 const db = mysql.createPool({
     host: 'localhost',
-    user: 'yeji',       // MySQL 사용자 이름
-    password: 'yj991010*', // MySQL 사용자 비밀번호
-    database: 'kcs_community', // 사용할 데이터베이스 이름
+    user: 'yeji',
+    password: 'yj991010*',
+    database: 'kcs_community',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
